fbc950e7e3235f439f45bcf5255176be554c8fc5
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-08-19 21:13:47 -0500

    created contact card component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,11 @@
-//import react 
+//import react
 import React from 'react';
+import ContactCard from './ContactCard';
 
 //App component
 const App = () => {
   return (
-    <div className='contact-card'>
-      <img src='https://via.placeholder.com/150' alt='image'/>
-      <div className='user-details'>
-        <p>Name: </p>
-        <p>Email: </p>
-        <p>Age: </p>
-      </div>
-    </div>
+    <ContactCard/>
   )
 }
 
---
+++ src/components/ContactCard.jsx
@@ -0,0 +1,17 @@
+//import react
+import React from 'react';
+
+const ContactCard = () => {
+  return (
+    <div className='contact-card'>
+      <img src='https://via.placeholder.com/150' alt='image' />
+      <div className='user-details'>
+        <p>Name: </p>
+        <p>Email: </p>
+        <p>Age: </p>
+      </div>
+    </div>
+  )
+}
+
+export default ContactCard;
