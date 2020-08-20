c82a6a437830d4fd572d47996485f0ccb11696b2
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-08-19 20:50:42 -0500

    create app component

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+//import react 
+import React from 'react';
+
+//App component
+const App = () => {
+  return(
+    <div>
+      I'm an App
+    </div>
+  )
+}
+
+//export App
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,10 @@
+//import react and its dom
 import React from 'react';
 import ReactDom from 'react-dom';
 
+//import app component
+import App from './components/App'
+
 const root = document.getElementById('app');
 
-ReactDom.render(
-  <h1>Hello x345</h1>, root
-)
+ReactDom.render(<App />, root);
